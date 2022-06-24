9811fd274228b665720dd1c5f11b0f2f2186be0a
Bahrul Ulum <91766508+Bahrul62@users.noreply.github.com>
2022-06-24 13:18:10 +0700

    Add files via upload

---
+++ dbs_server1.sql
@@ -0,0 +1,173 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 24 Jun 2022 pada 08.17
+-- Versi server: 10.4.22-MariaDB
+-- Versi PHP: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbs_server1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_barang_masuk`
+--
+
+CREATE TABLE `tb_barang_masuk` (
+  `id_transaksi` int(11) NOT NULL,
+  `tanggal` date NOT NULL,
+  `lokasi` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_barang_masuk`
+--
+
+INSERT INTO `tb_barang_masuk` (`id_transaksi`, `tanggal`, `lokasi`) VALUES
+(1, '2022-06-08', 'banyumas'),
+(2, '2022-06-08', 'purbalingga');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_detail_masuk`
+--
+
+CREATE TABLE `tb_detail_masuk` (
+  `id_transaksi` int(11) NOT NULL,
+  `kode_barang` int(11) NOT NULL,
+  `jumlah` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_detail_masuk`
+--
+
+INSERT INTO `tb_detail_masuk` (`id_transaksi`, `kode_barang`, `jumlah`) VALUES
+(1, 9, 20),
+(2, 9, 20),
+(1, 9, 50);
+
+--
+-- Trigger `tb_detail_masuk`
+--
+DELIMITER $$
+CREATE TRIGGER `detail_masuk` BEFORE INSERT ON `tb_detail_masuk` FOR EACH ROW UPDATE tb_master_barang SET
+ stok = stok+ NEW.jumlah
+WHERE
+kode_barang = NEW.kode_barang
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_master_barang`
+--
+
+CREATE TABLE `tb_master_barang` (
+  `kode_barang` int(11) NOT NULL,
+  `nama_barang` varchar(50) NOT NULL,
+  `satuan` varchar(50) NOT NULL,
+  `stok` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_master_barang`
+--
+
+INSERT INTO `tb_master_barang` (`kode_barang`, `nama_barang`, `satuan`, `stok`) VALUES
+(9, 'sosis', 'box', 230);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_user`
+--
+
+CREATE TABLE `tb_user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_user`
+--
+
+INSERT INTO `tb_user` (`id`, `username`, `password`) VALUES
+(1, 'admin', 'admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tb_barang_masuk`
+--
+ALTER TABLE `tb_barang_masuk`
+  ADD PRIMARY KEY (`id_transaksi`);
+
+--
+-- Indeks untuk tabel `tb_master_barang`
+--
+ALTER TABLE `tb_master_barang`
+  ADD PRIMARY KEY (`kode_barang`);
+
+--
+-- Indeks untuk tabel `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_barang_masuk`
+--
+ALTER TABLE `tb_barang_masuk`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_master_barang`
+--
+ALTER TABLE `tb_master_barang`
+  MODIFY `kode_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_user`
+--
+ALTER TABLE `tb_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `tb_detail_masuk`
+--
+ALTER TABLE `tb_detail_masuk`
+  ADD CONSTRAINT `fk_id_transaksi` FOREIGN KEY (`id_transaksi`) REFERENCES `tb_barang_masuk` (`id_transaksi`),
+  ADD CONSTRAINT `fk_kode` FOREIGN KEY (`kode_barang`) REFERENCES `tb_master_barang` (`kode_barang`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
